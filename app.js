d430d3bd92ed09ff3f35830086f76805df2da033
Chris Carrick <carrick.chris@gmail.com>
2021-02-13 14:13:14 -0700

    Added '/movies' endpoint. Implmented a 'get' endpoint that makes a request to TMDB to get the requested movies, and formated the respoonse based on the agreed rest contract. Added 'put', 'post', and 'delete' endpoints and made them return 404's. Updated tests to ensure tests were working properly. Added comments where needed. Made API_KEY an environment variable prior to pushing to git.

---
+++ app.js
@@ -0,0 +1,116 @@
+/**
+ * @fileoverview This our app. It serves as RestAPI webservice that is used to
+ * return formatted movie responses to the requester.
+ */
+const express = require("express");
+const request = require('request');
+const cors = require('cors')
+const app = express();
+const port = 3000;
+const API_KEY = process.env.TMDB_API_KEY;
+
+app.options('/movies', cors(), (_, res) => {
+    /**
+     * Define the 'OPTIONS' request for the `/movies` endpoint.
+     * @param req An express request object.
+     * @param res An express response object.
+     */
+    res.status(204).send();
+});
+
+app.get('/movies', cors(), (req, res) => {
+    /**
+     * Define the 'OPTIONS' request for the `/movies` endpoint.
+     * @param req An express request object.
+     * @param response An express response object.
+     */
+    const query_params = req.query
+    const search_parameter = query_params.search;
+    if (query_params === {} || !('search' in query_params)) {
+        // return a 400 response if there are no query parameters or if search
+        // is not in the query paramters.
+        res.status(400).send({error: 'missing search query parameter'});
+        return;
+    } else if (Object.size(query_params) > 1 || !query_params.search) {
+        // return a 400 respone if there are query parameters but 'search' is not one of them
+        // or if there are additional query parameters we were not expecting.
+        res.status(400).send({error: 'invalid query parameter'});
+        return;
+    } else if (!search_parameter) {
+        // the query parameter 'search' exisits but is undefined.
+        res.status(400).send({error: 'missing search query paramater value'});
+        return;
+    }
+
+    const the_movie_db_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${search_parameter}&page=1&include_adult=false`;
+    request({method: 'GET', uri:the_movie_db_url}, function(error, response, body) {
+        if (error) {
+            response.status(500).send({error: error})
+        }
+        let response_body = []
+        let body_json = JSON.parse(body);
+
+        for (var i = 0; i < 10; i++) {
+            if (!body_json.results) {
+                // Only add what we have. If we have less than 10 stop adding to response_body array.
+                break;
+            }
+
+            // Build the response object.
+            response_body.push({
+                movie_id: body_json.results[i].id,
+                title: body_json.results[i].title,
+                poster_image_url: `https://image.tmdb.org/t/p/w500/${body_json.results[i].poster_path}`,
+                popularity_summary: `${body_json.results[i].popularity} out of ${body_json.results[i].vote_count}`
+            })
+        }
+        res.status(200).send(response_body)
+        return;
+    })
+});
+
+app.post('/movies', cors(), (_, res) => {
+    /**
+     * Define the 'POST' request for the `/movies` endpoint.
+     * @param req An express request object.
+     * @param res An express response object.
+     */
+    res.status(404).send();
+});
+
+app.put('/movies', cors(), (_, res) => {
+    /**
+     * Define the 'PUT' request for the `/movies` endpoint.
+     * @param req An express request object.
+     * @param res An express response object.
+     */
+    res.status(404).send();
+});
+
+app.delete('/movies', cors(), (_, res) => {
+    /**
+     * Define the 'DELETE' request for the `/movies` endpoint.
+     * @param req An express request object.
+     * @param res An express response object.
+     */
+    res.status(404).send();
+});
+
+
+app.listen(port, () => {
+    /** Start the app. */
+    console.log(`App running. Listening on http://localhost:${port}`)
+    app.emit("app_started")
+});
+
+/**
+ * Borrowed from: https://stackoverflow.com/questions/5223/length-of-a-javascript-object
+ */
+Object.size = function(obj) {
+    var size = 0,
+      key;
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+  };
--- test/test_app.js
+++ test/test_app.js
@@ -1,14 +1,35 @@
 /**
  * @fileoverview Test file for app.js. It is used to verify that our apps endpoint
  * is properly configured and returns the correct responses.
-
  */
-const { request } = require('http');
+var expect = require("chai").expect;
+var assert = require('chai').assert;
+const request = require('request');
 
 const URL = 'http://localhost:3000/movies'
 
+
+describe("Verify ENV Vars", function() {
+    /** A test to verify that we have the correct env_variables set. */
+
+    it('Expects TMDB_API_KEY to exisit as an ENV variable.', function() {
+        expect(process.env.TMDB_API_KEY).is.not.undefined
+    })
+});
+
+
 describe("Endpoints", function() {
     /** A group of tests for our collection of endpoints */
+    var server;
+    before(function (done) {
+        server = require('../app');
+        done();
+    });
+
+    after(function(done) {
+        delete require.cache[require.resolve( '../app' )];
+        done();
+    });
 
     describe("GET - /movies", function() {
         /** A group of tests for the GET endpoint */
@@ -18,10 +39,9 @@ describe("Endpoints", function() {
              * Should return a 400 Bad Request if no query params are provided,
              * along with a message explaining what went wrong.
              */
-            request({method: 'GET', uri: URL}, function(error, response, body) {
-                console.log(error)
+            request({method: 'GET', uri: URL}, function(_, response, body) {
                 expect(response.statusCode).to.equal(400);
-                expect(body).to.equal("{'error': 'missing search query parameter'}");
+                expect(body).to.equal('{"error":"missing search query parameter"}');
                 done();
             });
         });
@@ -32,10 +52,22 @@ describe("Endpoints", function() {
              * Should return a 400 Bad Request when and invalid query parameter is provided,
              * along with a message explaining what went wrong.
              */
-            request({method: 'GET', uri: bad_get_url}, function(error, response, body) {
-                console.log(error);
+            request({method: 'GET', uri: bad_get_url}, function(_, response, body) {
                 expect(response.statusCode).to.equal(400);
-                expect(body).to.equal("{'error': 'invalid query parameter'}");
+                expect(body).to.equal('{"error":"invalid query parameter"}');
+                done();
+            });
+        });
+
+        bad_get_url = `${URL}?search`
+        it("returns statusCode: 400, body equals: {'error': 'invalid query parameter'}", function(done) {
+            /**
+             * Should return a 400 Bad Request when there is no query parameter provided,
+             * along with a message explaining what went wrong.
+             */
+            request({method: 'GET', uri: bad_get_url}, function(_, response, body) {
+                expect(response.statusCode).to.equal(400);
+                expect(body).to.equal('{"error":"invalid query parameter"}');
                 done();
             });
         });
@@ -45,7 +77,7 @@ describe("Endpoints", function() {
             /** Should return a 200 status code, and an array of no more than 10 movie objects. */
             request({method: 'GET', uri: good_get_url}, function(_, response, body) {
                 /**
-                 * movies objects should adhere to the following contract:
+                 * movie objects should adhere to the following contract:
                  * {
                  *      "movie_id": integer,
                  *      "title": string,
@@ -54,11 +86,12 @@ describe("Endpoints", function() {
                  * }
                  */
                 let body_json = JSON.parse(body)
-                let body_keys = ['movie_id', 'title', 'poster_img_url', 'popularity_summary']
+                let body_keys = ['movie_id', 'title', 'poster_image_url', 'popularity_summary']
                 expect(response.statusCode).to.equal(200);
-                expect(body_json.length).to.be.within(0, 10);
-                for (let i = 0; i <= body_json.length; i++){
-                    expect(body_json[i].length).to.only.have.keys(body_keys);
+                expect(body_json.length).to.be.within(5, 10);
+                assert.isTrue(body_json.length <= 10)
+                for (let i = 0; i < body_json.length; i++){
+                    expect(body_json[i]).to.have.keys(body_keys);
                 };
                 done();
             });
@@ -68,10 +101,10 @@ describe("Endpoints", function() {
     describe("OPTIONS - /movies", function() {
         /** A group of tests for the OPTIONS endpoint */
 
-        it("returns statusCode: 200, used for cors requests.", function(done) {
+        it("returns statusCode: 204, used for cors requests.", function(done) {
             request({method: 'OPTIONS', uri: URL}, function(_, response, _) {
                 /** Used to test that cors will work. */
-                expect(response.statusCode).to.equal(200);
+                expect(response.statusCode).to.equal(204);
                 done();
             });
         });
@@ -83,7 +116,7 @@ describe("Endpoints", function() {
 
         it("returns statusCode: 404 as this endpoint is not implemented.", function(done) {
             request({method: 'POST', uri: URL}, function(_, response, _) {
-                /** Used to test that cors will work. */
+                /** Used to test that post returns a 404. */
                 expect(response.statusCode).to.equal(404);
                 done();
             });
@@ -96,7 +129,7 @@ describe("Endpoints", function() {
 
         it("returns statusCode: 404 as this endpoint is not implemented.", function(done) {
             request({method: 'PUT', uri: URL}, function(_, response, _) {
-                /** Used to test that cors will work. */
+                /** Used to test that put returns a 404. */
                 expect(response.statusCode).to.equal(404);
                 done();
             });
@@ -108,11 +141,10 @@ describe("Endpoints", function() {
 
         it("returns statusCode: 404 as this endpoint is not implemented.", function(done) {
             request({method: 'DELETE', uri: URL}, function(_, response, _) {
-                /** Used to test that cors will work. */
+                /** Used to test that delete returns a 404. */
                 expect(response.statusCode).to.equal(404);
                 done();
             });
         });
-
-    })
+    });
 });
